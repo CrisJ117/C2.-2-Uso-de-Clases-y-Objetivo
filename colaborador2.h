1f2a610aef9cb06d12f56477134ecb45105cb52e
Kevin Antonio Tapuy Cañola <86172650+ke212002@users.noreply.github.com>
2021-07-15 22:25:06 -0500

    Update colaborador2.h
    creación del código para calcular la edad de una persona a partir de la fecha de nacimiento

--- colaborador2.h
+++ colaborador2.h
@@ -1,2 +1,19 @@
 class persona{
+    int main() 
+    {
+    int nacimiento;
+    int actual;
+    int edad;
+
+    printf("Ingrese su año de nacimiento: ");
+    scanf("%d", &nacimiento);
+
+    printf("Ingrese el año actual: ");
+    scanf("%d", &actual);
+
+    edad = actual - nacimiento;
+    printf("Usted tiene %d annos de edad\n", edad);
+
+    return 0;
+}
 };

6ee7d280086969b39e203d6bd9e1f19376eddf6f
Clarisa Ortiz Castillo <86218442+Clari07@users.noreply.github.com>
2021-07-16 15:08:55 -0500

    Update colaborador4.h
    Realice el punto numero 4, donde la accion que tendria que hacer el programa es toda la informacion ya compilada se tendría  que mostrar en un bloc de nota, el bloc de nota es la estencio .txt, entonce  el programa muestra y lee la informacion en un archivo .txt

--- colaborador4.h
+++ colaborador4.h
@@ -1,2 +1,47 @@
 class persona{
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+void GuardarArchivo(Persona per){
+	string nombre = per.GetNombre();
+	string apellido = per.GetApellido();
+	string cedula = per.GetCedula();
+	char nom[61];
+	char ape[61];
+	char ced[61];
+	for (int i=0; i<nombre.length(); i++){
+		nom[i] = nombre[i];
+		ape[i] = apellido[i];
+		ced[i] = cedula[i];
+	}
+	FILE* ptFichero;
+	ptFichero = fopen("persona.txt", "wt");
+	fprintf(ptFichero, "El nombre es %s\n", nom);
+	fprintf(ptFichero, "El apellido es %s\n", ape);
+	fprintf(ptFichero, "La cedula es %s\n", ced);
+	fprintf(ptFichero, "La fecha de nacimiento es %d\n", per.GetFecha());
+	fprintf(ptFichero, "El peso es %d\n", per.GetPeso());
+	fprintf(ptFichero, "La altura es %d\n", per.GetAltura());
+	fclose(ptFichero);
+	std::cout << "Los datos han sido guardados correctamente\n\n";
+}
+
+void LeerArchivo(){
+    
+    FILE* ficheroU;
+	char frase[61];
+	ficheroU = fopen("persona.txt", "rt");
+	std::cout << "\n";
+	std::cout << "Los datos personales obtenidos son:\n\n";
+    std::cout << "\n";
+	do{
+    	fgets(frase, 60, ficheroU);
+    	puts(frase);
+	}while (!feof(ficheroU));
+	fclose(ficheroU);
+	getchar();
+}
 };

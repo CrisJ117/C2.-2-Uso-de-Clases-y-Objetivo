0610dad1542409071b28a7cd7522b4425a026999
Cristopher Justyn Tigua Vivero <86115235+CrisJ117@users.noreply.github.com>
2021-07-16 13:28:12 -0500

    Update colaborador5.h
    Este class comienza desde la linea 1 y termina en la 39 se encarga de recuperar la información  que se guarda en colaborador4.h, utilice constructor y destructor.

--- colaborador5.h
+++ colaborador5.h
@@ -1,2 +1,39 @@
-class persona{
+#include<iostream>
+using namespace std;
+#ifndef colaborador5_h
+#define colaborador5.h
+
+class Persona {
+	
+	public:
+	
+		//constructor y destructor
+		Persona() {};
+	
+	private:
+	
+		string nombre;
+		string apellido;
+		string cedula;
+		int fecha;
+		int peso;
+		int altura;
+	
+	public:
+	
+		void SetNombre(string n) { nombre=n; }
+		void SetApellido(string a) { apellido=a; }
+		void SetCedula(string c) { cedula=c; }
+		void SetFecha(int f) { fecha=f; }
+		void SetPeso( int p) { peso = p; }
+		void SetAltura( int a ) { altura = a; }
+		const string GetNombre( ){ return nombre; }
+		const string GetApellido( ){ return apellido; }
+		const string GetCedula( ){ return cedula; }
+		const int GetFecha( ){ return fecha; }
+		const int GetPeso( ){ return peso; }
+		const int GetAltura( ){ return altura; }
+	
 };
+
+#endif

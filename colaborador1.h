0663c3b07b17f621bfea415b05c68387718a9f45
genesi Simisterra <86116060+SAMY69xx@users.noreply.github.com>
2021-07-16 00:28:55 -0500

    Update colaborador1.h
    update me

--- colaborador1.h
+++ colaborador1.h
@@ -1,2 +1,27 @@
-class persona{
-};
+#include<iostream>
+using namespace std;
+#include "Persona.h"
+
+
+void GetInfor(Persona per){
+	std::cout << "\n";
+	std::cout << "Los datos personales son:\n\n";
+    std::cout << "\n";
+	std::cout << "Nombre: " << per.GetNombre() << '\n';
+	std::cout << "Apellido: " << per.GetApellido() << '\n';
+	std::cout << "Cedula: " << per.GetCedula() << '\n';
+	std::cout << "Fecha: " << per.GetFecha() << '\n';
+	std::cout << "Peso: " << per.GetPeso() << '\n';
+	std::cout << "Altura: " << per.GetAltura() << '\n';	
+}
+
+Persona PosInfor(string nom,string ape,string ced,int fec,int pes,int alt){
+	Persona per = Persona();
+	per.SetNombre(nom);
+	per.SetApellido(ape);
+	per.SetCedula(ced);
+	per.SetFecha(fec);
+	per.SetPeso(pes);
+	per.SetAltura(alt);
+	return per;
+}

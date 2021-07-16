9174a336a37932266728705cb9009ac0ac8f8b4a
genesi Simisterra <86116060+SAMY69xx@users.noreply.github.com>
2021-07-15 19:06:34 -0500

    Update Principal.cpp
    update

--- Principal.cpp
+++ Principal.cpp
@@ -8,6 +8,59 @@ using namespace std;
 #include "colaborador4.h"
 #include "colaborador5.h"
 
+class Persona {
+	public:
+	
+	//constructor y destructor
+	Persona() {};
+	
+	private:
+	
+	char nombre;
+	char apellido;
+	char cedula;
+	char fecha;
+	int peso;
+	int altura;
+	
+	public:
+	
+	void SetNombre(char n) { nombre=n; }
+	void SetApellido(char a) { apellido=a; }
+	void SetCedula(char c) { cedula=c; }
+	void SetFecha(char f) { fecha=f; }
+	void SetPeso( int p) { peso = p; }
+	void SetAltura( int a ) { altura = a; }
+	const char GetNombre( ){ return nombre; }
+	const char GetApellido( ){ return apellido; }
+	const char GetCedula( ){ return cedula; }
+	const char GetFecha( ){ return fecha; }
+	const int GetPeso( ){ return peso; }
+	const int GetAltura( ){ return altura; }
+	
+	
+	Persona PosInfor(char nom,char ape,char ced,char fec,int pes,int alt){
+		Persona per = Persona();
+		per.SetNombre(nom);
+		per.SetApellido(ape);
+		per.SetCedula(ced);
+		per.SetFecha(fec);
+		per.SetPeso(pes);
+		per.SetAltura(alt);
+		return per;
+	}
+	
+	void GetInfor(Persona per){
+		std::cout << "Nombre: " << per.GetNombre() << '\n';
+		std::cout << "Apellido: " << per.GetApellido() << '\n';
+		std::cout << "Cedula: " << per.GetCedula() << '\n';
+		std::cout << "Fecha: " << per.GetFecha() << '\n';
+		std::cout << "Peso: " << per.GetPeso() << '\n';
+		std::cout << "Altura: " << per.GetAltura() << '\n';
+	}
+	
+};
+
 int main()
 {
     int opcion;
@@ -35,8 +88,27 @@ int main()
         
         switch (opcion) {
             case 1:
-               
-                system("pause>nul");
+               Persona per;
+	
+		char nom,ape,ced,fec;
+		int pes,alt;
+
+		std::cout << "Ingrese el nombre\n";
+		std::cin >> nom;
+		std::cout << "Ingrese el apellido\n";
+		std::cin >> ape;
+		std::cout << "Ingrese la cedula\n";
+		std::cin >> ced;
+		std::cout << "Ingrese la fecha de nacimiento\n";
+		std::cin >> fec;
+		std::cout << "\nIngrese el peso\n";
+		std::cin >> pes;
+		std::cout << "\nIngrese la altura\n";
+		std::cin >> alt;
+
+		per = per.PosInfor(nom,ape,ced,fec,pes,alt);
+
+		per.GetInfor(per);
                 break;
                 
             case 2:
